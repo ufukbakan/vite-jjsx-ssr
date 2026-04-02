7a1aeae0db0305448db475941218832154c961cb
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-04-02 22:24:43 +0300

    upgrade latest @carats/express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import { router } from '@carats/express'
+import { carats } from '@carats/express'
 import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 5173
 
-app.use(router)
+app.use(carats())
 
 const server = app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
