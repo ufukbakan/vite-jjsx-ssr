514e2a45ee8b6c198a5f89d06a20fd5f33ed724f
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-05-02 23:08:44 +0300

    fix error handling

--- src/server/entrypoint.ts
+++ src/server/entrypoint.ts
@@ -2,7 +2,7 @@ import { carats } from '@carats/express';
 import { defineServerEntry } from '@carats/ssr';
 import express from 'express';
 import facets from '../client/facets';
-import './culets'
+import './culets';
 
 
 export const segments = defineServerEntry(facets) // must export segments for @carats/express
@@ -13,5 +13,9 @@ const port = process.env.PORT || 5173
 app.use(carats())
 
 export const server = app.listen(port, (err) => { // must export server for @carats/ssg
-  !err && console.log(`Server started at http://localhost:${port}`)
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server started at http://localhost:${port}`)
 })
