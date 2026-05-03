db5ffa98f6e5474378467c97fd8150fb6cee8421
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-05-03 21:55:58 +0300

    fix dev server problems

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import { chdir } from "process";
-chdir(import.meta.dirname);
-export default import('./server/entrypoint')
+import { carats } from '@carats/express';
+import express from 'express';
+
+const app = express()
+const port = process.env.PORT || 5173
+
+app.use(carats())
+
+export default app.listen(port, (err) => { // must export server for @carats/ssg
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server started at http://localhost:${port}`)
+})
--- src/server/entrypoint.ts
+++ src/server/entrypoint.ts
@@ -1,21 +1,5 @@
-import { carats } from '@carats/express';
 import { defineServerEntry } from '@carats/ssr';
-import express from 'express';
 import facets from '../client/facets';
 import './culets';
 
-
-export const segments = defineServerEntry(facets) // must export segments for @carats/express
-
-const app = express()
-const port = process.env.PORT || 5173
-
-app.use(carats())
-
-export const server = app.listen(port, (err) => { // must export server for @carats/ssg
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server started at http://localhost:${port}`)
-})
+export default defineServerEntry(facets) // must export segments for @carats/express
